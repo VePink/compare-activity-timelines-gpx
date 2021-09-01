c6266b02b2ad81a9e608eab74af0254be793026b
Vėjas Pinkevičius <vejaspink@gmail.com>
2021-09-01 13:39:44 +0300

    First working version

--- compare-activity-timelines-gpx.py
+++ compare-activity-timelines-gpx.py
@@ -3,11 +3,14 @@ import numpy as np
 from gpx_converter import Converter
 from geopy.distance import lonlat, distance
 
-input_GPX_file = 'C:\\Users\\Ve\\Documents\\GitHub\\compare-activity-timelines-gpx\\input_GPXs\\test2.gpx'
+input_GPX_file = 'C:\\Users\\Ve\\Documents\\GitHub\\compare-activity-timelines-gpx\\input_GPXs\\test1.gpx'
 original_df = Converter(input_file=input_GPX_file).gpx_to_dataframe()
 original_df.drop(columns=['altitude'], inplace=True)
 
-deltas_list = []
+deltas_list = [{
+            'delta_dist_m': 0.0,
+            'delta_time': '0 days 00:00:00'
+            }]
 
 for index, row in original_df.iterrows():
     try:
@@ -25,7 +28,7 @@ for index, row in original_df.iterrows():
 
         deltas_list.append(
             {
-            'delta_distance_m': delta_distance_m,
+            'delta_dist_m': delta_distance_m,
             'delta_time': delta_time
             }
         )
@@ -34,17 +37,22 @@ for index, row in original_df.iterrows():
          print('Index error detected, as expected.')
 
 deltas_df = pd.DataFrame(deltas_list)
+
 print(deltas_df)
+print("=========================")
+print(original_df)
 
 
+result_df = pd.merge(original_df, deltas_df, left_index=True, right_index=True)
 
-'''
+# Cumulative sum
+result_df['dist_cumulative_sum_m'] = result_df.delta_dist_m.cumsum()
+result_df['dist_cumulative_time'] = result_df.delta_time.cumsum()
+
+print(result_df)
+
 import matplotlib.pyplot as plt
-
-# gca stands for 'get current axis'
 ax = plt.gca()
-
-result_df.plot(kind='line',x='delta_time',y='delta_distance_m', color='red', ax=ax)
+result_df.plot(kind='line',x='dist_cumulative_time',y='dist_cumulative_sum_m', color='red', ax=ax)
 
 plt.show()
-'''
